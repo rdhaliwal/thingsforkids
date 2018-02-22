e3bd7f465f36cc722389495809428677edf6580e
Ahmad Saleem <ahmadsaleem93@gmail.com>
2018-02-22 15:59:11 +0500

    Misc styling fixes and updates.

--- app/admin/listings.rb
+++ app/admin/listings.rb
@@ -39,10 +39,10 @@ ActiveAdmin.register Listing do
     f.inputs :highchairs
     f.inputs :baby_change_room
     f.inputs do
-      f.input :opens_at, as: :date_picker
+      f.input :opens_at, as: :time_picker
     end
     f.inputs do
-      f.input :closes_at, as: :date_picker
+      f.input :closes_at, as: :time_picker
     end
     f.inputs :address
     f.inputs :city
