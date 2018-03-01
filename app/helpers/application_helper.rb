0840f334f9f22c2e26dc1b28457786aaad930206
Ahmad Saleem <ahmadsaleem93@gmail.com>
2018-03-01 05:27:11 +0500

    Add social media links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def facebook_url
+    "https://www.facebook.com/thingsforkidsau/"
+  end
+
+  def instagram_url
+    "https://www.instagram.com/thingsforkidsau/"
+  end
 end
