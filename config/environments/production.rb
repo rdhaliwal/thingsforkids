1a23624a3d3d7720b11af84124a6cc3a69a4d71e
Ahmad Saleem <ahmadsaleem93@gmail.com>
2018-03-03 07:53:01 +0500

    Set domain for sendgrid.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
+    domain: 'things-for-kids.herokuapp.com',
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
