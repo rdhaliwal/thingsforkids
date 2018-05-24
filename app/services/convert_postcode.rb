b787c6a030ad808fbbda93a3e0a42289e842bc9b
Ahmad Saleem <ahmadsaleem93@gmail.com>
2018-05-24 08:33:56 +0500

    Fix postcode logic

--- app/services/convert_postcode.rb
+++ app/services/convert_postcode.rb
@@ -4,14 +4,14 @@ class ConvertPostcode
   URL = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyARuFVkZ7z_nGJQbPfySrhhqEOsVFYj0vI&address="
 
   def self.call postcode
-    result = HTTParty.get "#{URL}#{postcode},country=aus"
+    result = HTTParty.get "#{URL}#{postcode},aus"
     if result["status"] == "OK"
       location = result["results"].first["geometry"]["location"]
       latitude = location["lat"]
       longitude = location["lng"]
       [latitude, longitude]
     else
-      [latitude, longitude]
+      [-37.8152065, 144.963937]
     end
   end
 end
