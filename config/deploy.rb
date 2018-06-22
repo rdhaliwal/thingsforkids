c8ca838ea3c47bc3b23b1c1f952bbb5dc7efe830
Ahmad Saleem <ahmadsaleem93@gmail.com>
2018-06-22 12:43:10 +0500

    Hardcode socket file name

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/var/www/thingsforkids"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/thingsforkids-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
