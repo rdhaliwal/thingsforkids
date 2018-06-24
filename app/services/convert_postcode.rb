81be7f3a59603b93b631b3c0a874538b7ab3d3fb
Ahmad Saleem <ahmadsaleem93@gmail.com>
2018-06-24 12:53:36 +0500

    Fix minor bugs.

--- app/services/convert_postcode.rb
+++ app/services/convert_postcode.rb
@@ -4,7 +4,7 @@ class ConvertPostcode
   URL = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyARuFVkZ7z_nGJQbPfySrhhqEOsVFYj0vI&address="
 
   def self.call postcode
-    result = HTTParty.get "#{URL}#{postcode},country=aus"
+    result = HTTParty.get "#{URL}#{postcode},aus"
     if result["status"] == "OK"
       location = result["results"].first["geometry"]["location"]
       latitude = location["lat"]
