28ffb8adb637ee28e7b14dcccbdb3fbef2da6d7a
Talha Junaid <talha.junaid65@amblersaleem.com>
2018-07-04 17:52:22 +0500

    Integrate resque.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
+  include Resque::Mailer
   default from: 'hello@thingsforkids.com.au'
   include ApplicationHelper
   add_template_helper(ApplicationHelper)
