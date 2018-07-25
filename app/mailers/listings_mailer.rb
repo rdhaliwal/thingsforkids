e4bc49efe0001a1ca40a5b854eef80c01f96d4fe
Ahmad Saleem <ahmadsaleem93@gmail.com>
2018-07-25 11:50:32 +0500

    Incorporate some feedback.

--- app/mailers/listings_mailer.rb
+++ app/mailers/listings_mailer.rb
@@ -1,16 +1,16 @@
 class ListingsMailer < ApplicationMailer
   def free_listing(listing_id)
     @listing = Listing.find(listing_id)
-    mail(to: @listing.email, subject: "Thank you for creating a listing on things for kids.")
+    mail(to: @listing.user_email, subject: "Thank you for creating a listing on things for kids.")
   end
 
   def premium_listing(listing_id)
     @listing = Listing.find(listing_id)
-    mail(to: @listing.email, subject: "Thank you for creating your listing on things for kids.")
+    mail(to: @listing.user_email, subject: "Thank you for creating your listing on things for kids.")
   end
 
   def upgraded_to_premium(listing_id)
     @listing = Listing.find(listing_id)
-    mail(to: @listing.email, subject: "Thank you for upgrading your listing on things for kids.")
+    mail(to: @listing.user_email, subject: "Thank you for upgrading your listing on things for kids.")
   end
 end
